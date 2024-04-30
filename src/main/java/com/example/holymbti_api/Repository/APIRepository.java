77b9851d1d6b8908ab11c9927a49ef6305e52d53
spzspz@live.com <spzspz@live.com>
2024-04-30 20:39:40 +0900

    CHORE : 주석 철자 교정

--- src/main/java/com/example/holymbti_api/Repository/APIRepository.java
+++ src/main/java/com/example/holymbti_api/Repository/APIRepository.java
@@ -12,7 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public interface APIRepository extends JpaRepository<MBTI,Long> {
 
-    //비워있어도 잘 작동함.
+    //비어있어도 잘 작동함.
     // long 이 아니라 Long으로 작성. ex) int => Integer 같이 primitive형식 사용못함
 
     // findBy뒤에 컬럼명을 붙여주면 이를 이용한 검색이 가능하다
